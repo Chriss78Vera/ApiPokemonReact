3f97d955e63eadc6451652e979055e5b4b928342
Chriss78Vera <chrissvera75@gmail.com>
2022-10-22 23:43:43 -0500

    Documentacion en swagger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const pokemonRoutes =require('./routes/pokemon')
-
+const pokemonRoutes = require("./routes/pokemon");
+const path = require("path");
+// swagger is
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerSpec = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "PokemonAPI MongoDB",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:9000",
+      },
+    ],
+  },
+  apis: [`${path.join(__dirname, "./routes/*.js")}`],
+};
+// Middleware
 const app = express();
 const port = process.env.PORT || 9000;
+app.use(
+  "/api-doc",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerJsDoc(swaggerSpec))
+);
 
 app.use(express.json());
-app.use('/api',pokemonRoutes);
+app.use("/api", pokemonRoutes);
 
 //MongoDb Connection
 mongoose
--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -3,17 +3,105 @@ const pokemonSchema = require("../models/pokemon");
 
 const router = express.Router();
 
-/* Creating a new pokemon in the database. */
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Pokemon:
+ *      type: object
+ *      properties:
+ *        name:
+ *          type: string
+ *          description: Pokemon name
+ *        id:
+ *          type: string
+ *          description: Pokemon number
+ *        _id:
+ *          type: string
+ *          description: Pokemon identifier
+ *        height:
+ *          type: string
+ *          description: Pokemon height
+ *        weight:
+ *          type: string
+ *          description: Pokemon weight
+ *        sprites:
+ *          type: array
+ *          description: Pokemon Image
+ *          items:
+ *           other:
+ *             home:
+ *               front_default:
+ *        types:
+ *          type: array
+ *          description: Pokemon type
+ *          items:
+ *             type: object
+ *             propierties:
+ *              type:
+ *                name: 
+ *                  type: string
+ *                  description: Pokemon type
+ *      required:
+ *        - name
+ *        - id
+ *        - _id
+ *      example:
+ *        name: Pikachu
+ *        id: 25
+ *        _id: 25
+ *        height: 4
+ *        weight: 60
+ *        sprites:
+ *          other:
+ *            home:
+ *              front_default: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/25.png
+ *        types:
+ *          - type:
+ *              name: electric
+ */       
+
+/**
+ * @swagger
+ * /api/pokemon:
+ *  post:
+ *    summary: Create a new pokemon
+ *    tags: [Pokemon]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/Pokemon'
+ *    responses:
+ *      200:
+ *        description: New pokemon was been created!
+ */
 router.post("/pokemon", (req, res) => {
   const pokemon = pokemonSchema(req.body);
-      pokemon
-        .save()
-        .then((data) => res.json(data))
-        .catch((err) => res.json({ message: err.message }));
-        console.log("Pokemon saved")
+  pokemon
+    .save()
+    .then((data) => res.json(data))
+    .catch((err) => res.json({ message: err.message }));
+  console.log("Pokemon saved");
 });
-
-/* This is a route that is used to get all the pokemon in the database. */
+/**
+ * @swagger
+ * /api/pokemon:
+ *  get:
+ *    summary: return all pokemons saved in data base
+ *    tags: [Pokemon]
+ *    responses:
+ *      200:
+ *        description: Get all the pokemon in the database.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Pokemon'
+ */
 router.get("/pokemon", (req, res) => {
   pokemonSchema
     .find()
@@ -21,6 +109,5 @@ router.get("/pokemon", (req, res) => {
     .catch((err) => res.json({ message: err.message }));
   console.log("Lectura de la base");
 });
-
 
 module.exports = router;
